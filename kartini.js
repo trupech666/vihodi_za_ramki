0b03ebfa94cdc98fd62002b5e072acfeb39c663e
trupech666 <meshkova.marina.08@gmail.com>
2026-05-21 23:34:01 +0300

    Add files via upload

---
+++ kartini.js
@@ -0,0 +1,34 @@
+const btn2 = document.getElementById("button2");
+const text2 = document.getElementById("factText2");
+btn2.addEventListener("click", function() {
+  text2.classList.toggle("show");
+  btn2.classList.toggle("rotated");
+});
+
+const btn3 = document.getElementById("button3");
+const text3 = document.getElementById("factText3");
+btn3.addEventListener("click", function() {
+  text3.classList.toggle("show");
+  btn3.classList.toggle("rotated");
+});
+
+const btn4 = document.getElementById("button4");
+const text4 = document.getElementById("factText4");
+btn4.addEventListener("click", function() {
+  text4.classList.toggle("show");
+  btn4.classList.toggle("rotated");
+});
+
+const btn5 = document.getElementById("button5");
+const text5 = document.getElementById("factText5");
+btn5.addEventListener("click", function() {
+  text5.classList.toggle("show");
+  btn5.classList.toggle("rotated");
+});
+
+const btn6 = document.getElementById("button6");
+const text6 = document.getElementById("factText6");
+btn6.addEventListener("click", function() {
+  text6.classList.toggle("show");
+  btn6.classList.toggle("rotated");
+});
---
+++ mySite.js
@@ -0,0 +1,29 @@
+const sendBtn = document.getElementById("send");
+const clearBtn = document.getElementById("clear");
+const nameInput = document.getElementById("FirstName");
+const lastNameInput = document.getElementById("LastName");
+const emailInput = document.getElementById("email");
+const commInput = document.getElementById("comm");
+
+clearBtn.onclick = function() {
+  nameInput.value = "";
+  lastNameInput.value = "";
+  emailInput.value = "";
+  commInput.value = "";
+};
+	
+sendBtn.onclick = function() {
+  if (nameInput.value === "") {
+    alert("Введите имя");
+  } else if (lastNameInput.value === "") {
+    alert("Введите фамилию");
+  } else if (emailInput.value === "") {
+    alert("Введите email");
+  } else {
+    alert("Спасибо, " + nameInput.value + "! Сообщение отправлено.");
+    nameInput.value = "";
+    lastNameInput.value = "";
+    emailInput.value = "";
+    commInput.value = "";
+  }
+};
